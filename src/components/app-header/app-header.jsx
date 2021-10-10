8d7f707e45d8f7b04fe88525bb238b1c2d15cdd3
zolotovdy <zolotovdy@ugpa.ru>
2021-10-10 21:05:31 +0300

    Переименование расширения из js в jsx для компонентов. Заглушки для компонентов добавлены.

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export class AppHeader extends React.Component {
+    render(){
+        return <h1>Hello World</h1>
+    }
+}
---
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export class BurgerIngredients extends React.Component {
+    render(){
+        return <h1>Hello World</h1>
+    }
+}
