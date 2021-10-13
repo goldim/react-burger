efe2b6c7e80b9e798f709b9e17edd6340d3d1af7
goldim1993 <goldim1993@gmail.com>
2021-10-13 11:36:21 +0300

    Добавлен DragItem для элементов конструктора. Элемент конструктора занимает всю доступную длину родителя.

--- src/components/burger-constructor/chosen-ingredient.jsx
+++ src/components/burger-constructor/chosen-ingredient.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ConstructorElement } from '../../utils/yandex-components'
+import { ConstructorElement, DragIcon } from '../../utils/yandex-components'
 import styles from './burger-constructor.module.css'
 
 export class ChosenIngredient extends React.Component {
@@ -13,8 +13,13 @@ export class ChosenIngredient extends React.Component {
         return (<>{baseLabel}<br/>{result}</>);
     }
 
+    isCenter = (type) => {
+        return !["bottom", "top"].includes(type);
+    }
+
     render = () => (
         <div className={styles.chosenItem}>
+            { this.isCenter(this.props.type) && <DragIcon/> }
             <ConstructorElement
                 type={this.props.type}
                 isLocked={this.props.isLocked}
