0a6b0f4f617a975d8e3fd83205106db05eca6f49
goldim1993 <goldim1993@gmail.com>
2021-10-13 11:57:20 +0300

    Свободное место между итоговой ценой с валютой и кнопной оформить заказ

--- src/components/burger-constructor/total-bar.jsx
+++ src/components/burger-constructor/total-bar.jsx
@@ -5,11 +5,13 @@ import styles from './burger-constructor.module.css'
 export class TotalBar extends React.Component {
     render = () => (
         <p className={styles.totalBar}>
-            <span className="text text_type_digits-medium">
-                { this.props.totalPrice }
-            </span>
-            <CurrencyIcon/>
-            <Button className={styles.makeOrderBtn} size="large">Оформить заказ</Button>
+            <div className={styles.priceCurrency}>
+                <span className="text text_type_digits-medium">
+                    { this.props.totalPrice }
+                </span>
+                <CurrencyIcon/>
+            </div>
+            <Button size="large">Оформить заказ</Button>
         </p>
     )
 }
