42c0bb8a28903c265af89f5922eb70b9106efa5e
zolotovdy <zolotovdy@ugpa.ru>
2021-10-15 13:03:30 +0300

    Данные грузятся из удаленного хранилища

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,15 +4,35 @@ import { AppHeader } from '../app-header/app-header';
 import { BurgerConstructor } from '../burger-constructor/burger-constructor'
 import { BurgerIngredients } from '../burger-ingredients/burger-ingredients'
 
-import ingredientModel from '../../utils/data.json'
-
 function App() {
+  const [ingredients, setIngredients] = React.useState([]);
+
+  const onIngredientsLoaded = async (response: any) => {
+    const parsed = await response.json();
+    if (parsed.success){
+      setIngredients(parsed.data);
+    }
+  }
+  
+  const onIngredientsLoadingFailed = () => {
+    console.log('failed');
+  }
+  
+  const loadIngredients = () => {
+    const INGREDIENTS_URL = 'https://norma.nomoreparties.space/api/ingredients';
+    fetch(INGREDIENTS_URL)
+      .then(onIngredientsLoaded)
+      .catch(onIngredientsLoadingFailed);
+  }
+
+  React.useEffect(loadIngredients, []);
+
   return (
     <div className={ appStyles.App }>
       <AppHeader/>
       <main>
-        <BurgerIngredients model={ingredientModel}/>
-        <BurgerConstructor model={ingredientModel}/>
+        <BurgerIngredients model={ingredients}/>
+        <BurgerConstructor model={ingredients}/>
       </main>
     </div>
   );
