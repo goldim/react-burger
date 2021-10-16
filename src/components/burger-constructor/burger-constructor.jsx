4182ae4d8df129f55e54f492706b48a837fde045
zolotovdy <zolotovdy@ugpa.ru>
2021-10-17 00:23:19 +0300

    Переделка на функциональный компонент

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
 import PropTypes from 'prop-types'
 
 import TotalBar from './total-bar';
 import { IngredientList } from './ingredient-list';
 import constructorStyles from './burger-constructor.module.css';
 
-export class BurgerConstructor extends React.Component {
-    calcTotalPrice = () => {
-        return this.props.model.reduce((acc, current) => acc + current.price, 0);
+const BurgerConstructor = ({data}) => {
+    const calcTotalPrice = () => {
+        return data.reduce((acc, current) => acc + current.price, 0);
     }
 
-    render = () => (
+    return (
         <section className={constructorStyles.burgerConstructor}>
-            <IngredientList ingredients={this.props.model}/>
-            <TotalBar totalPrice={this.calcTotalPrice()}/>
+            <IngredientList ingredients={data}/>
+            <TotalBar totalPrice={calcTotalPrice()}/>
         </section>
     )
 }
 
+// const menuItemPropTypes = PropTypes.shape({
+//     _id: PropTypes.string.isRequired,
+//     name: PropTypes.string.isRequired,
+//     type: PropTypes.string.isRequired,
+//     proteins: PropTypes.number.isRequired,
+//     fat: PropTypes.number.isRequired,
+//     carbohydrates: PropTypes.number.isRequired,
+//     calories: PropTypes.number.isRequired,
+//     price: PropTypes.number.isRequired,
+//     image: PropTypes.string.isRequired,
+//     image_mobile: PropTypes.string.isRequired,
+//     image_large: PropTypes.string.isRequired,
+//     __v: PropTypes.number.isRequired,
+// });
+
 BurgerConstructor.propTypes = {
-    model: PropTypes.array
+    data: PropTypes.array
 }
+
+export default BurgerConstructor;
