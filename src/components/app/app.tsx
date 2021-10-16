2593ee1b99e3a7de539b9629dd46320f8c8f1415
zolotovdy <zolotovdy@ugpa.ru>
2021-10-17 00:22:23 +0300

    Получение данных через fetch переделан на использование только async  и await. При ошибке не выкидывается исключение, ошибка фиксируется в журнале. Также в этот файл добавлен блок для хранения модальных окон.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,40 +1,49 @@
 import React from 'react';
 import appStyles from './app.module.css';
 import { AppHeader } from '../app-header/app-header';
-import { BurgerConstructor } from '../burger-constructor/burger-constructor'
+import BurgerConstructor from '../burger-constructor/burger-constructor'
 import { BurgerIngredients } from '../burger-ingredients/burger-ingredients'
+
+const INGREDIENTS_SOURCE = 'https://norma.nomoreparties.space/api/ingredients';
 
 function App() {
   const [ingredients, setIngredients] = React.useState([]);
 
-  const onIngredientsLoaded = async (response: any) => {
-    const parsed = await response.json();
-    if (parsed.success){
-      setIngredients(parsed.data);
+  const fetchIngredients = async (url: string) => {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const json = await response.json();
+      setIngredients(json.data);
+    } else {
+      throw new Error(response.status.toString());
     }
   }
-  
-  const onIngredientsLoadingFailed = () => {
-    throw Error("loading of data failed");
-  }
-  
-  const loadIngredients = () => {
-    const INGREDIENTS_URL = 'https://norma.nomoreparties.space/api/ingredients';
-    fetch(INGREDIENTS_URL)
-      .then(onIngredientsLoaded)
-      .catch(onIngredientsLoadingFailed);
-  }
 
-  React.useEffect(loadIngredients, []);
+  React.useEffect(() => { 
+    const loadIngredients = async () => {
+      try {
+        await fetchIngredients(INGREDIENTS_SOURCE);
+      }
+      catch (e){
+        console.log(e);
+      }
+    }
+
+    loadIngredients() 
+  }, []);
 
   return (
-    <div className={ appStyles.App }>
-      <AppHeader/>
-      <main>
-        <BurgerIngredients model={ingredients}/>
-        <BurgerConstructor model={ingredients}/>
-      </main>
-    </div>
+    <>
+      <div className={ appStyles.App }>
+        <AppHeader/>
+        <main>
+          <BurgerIngredients model={ingredients}/>
+          <BurgerConstructor data={ingredients}/>
+        </main>
+      </div>
+      <div id="react-modals"/>
+    </>
   );
 }
 
