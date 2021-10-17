2dc6f74701b0c584cb244b1d52caeff552913034
zolotovdy <zolotovdy@ugpa.ru>
2021-10-17 12:36:26 +0300

    Добавлена проверка типов.  Деструктуризация props.

--- src/components/ingredient-details.jsx/ingredient-details.jsx
+++ src/components/ingredient-details.jsx/ingredient-details.jsx
@@ -3,31 +3,39 @@ import PropTypes from 'prop-types';
 
 import styles from './ingredient-details.module.css'
 
-const SmallText = React.memo((props) => (
-    <p className="text text_type_main-small text_color_inactive">{props.children}</p>
+const SmallText = React.memo(({children}) => (
+    <p className="text text_type_main-small text_color_inactive">{children}</p>
 ));
 
-const InactiveNumber = React.memo((props) => (
-    <p className="text text_type_digits-default text_color_inactive">{props.children}</p>
+SmallText.propTypes = {
+    children: PropTypes.string.isRequired
+}
+
+const InactiveNumber = React.memo(({children}) => (
+    <p className="text text_type_digits-default text_color_inactive">{children}</p>
 ));
 
-const IngredientDetails = (props) => (
+InactiveNumber.propTypes = {
+    children: PropTypes.number.isRequired
+}
+
+const IngredientDetails = ({name, image, calories, proteins, fat, carbohydrates}) => (
     <div className={styles.container}>
         <div>
             <div>
-                <img src={props.image} alt={props.name}/>
+                <img src={image} alt={name}/>
                 <p className={`${styles.name} text text_type_main-medium`}>
-                    {props.name}
+                    {name}
                 </p>
                 <div className={styles.subInfo}>
                     <SmallText>Калории, ккал</SmallText>
                     <SmallText>Белки, г</SmallText>
                     <SmallText>Жиры, г</SmallText>
                     <SmallText>Углеводы, г</SmallText>
-                    <InactiveNumber>{props.calories}</InactiveNumber>
-                    <InactiveNumber>{props.proteins}</InactiveNumber>
-                    <InactiveNumber>{props.fat}</InactiveNumber>
-                    <InactiveNumber>{props.carbohydrates}</InactiveNumber>
+                    <InactiveNumber>{calories}</InactiveNumber>
+                    <InactiveNumber>{proteins}</InactiveNumber>
+                    <InactiveNumber>{fat}</InactiveNumber>
+                    <InactiveNumber>{carbohydrates}</InactiveNumber>
                 </div>
             </div>
         </div>
