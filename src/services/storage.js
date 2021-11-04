20035f24aedc995fa371377983fce5bc5c252bde
zolotovdy <zolotovdy@ugpa.ru>
2021-11-04 16:08:13 +0300

    Подключены DevTools

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,5 +1,12 @@
-import { applyMiddleware, createStore } from 'redux'
+import { compose, applyMiddleware, createStore } from 'redux'
 import { rootReducer } from './reducers/root'
 import thunk from 'redux-thunk'
 
-export const ReduxStore = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose; 
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+export const ReduxStore = createStore(rootReducer, enhancer);
