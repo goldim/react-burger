b27a68f1497b742d8d414eec234bc7869ae1795f
zolotovdy <zolotovdy@ugpa.ru>
2022-01-09 12:22:11 +0300

    Действия типизированы по требованию

--- src/services/constants/index.ts
+++ src/services/constants/index.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'norma.nomoreparties.space';
-export const WS_BASE_URL = `wss://${BASE_URL}`;
-export const HTTPS_BASE_URL = `https://${BASE_URL}`;
+export const BASE_URL = 'norma.nomoreparties.space'
+export const WS_BASE_URL = `wss://${BASE_URL}`
+export const HTTPS_BASE_URL = `https://${BASE_URL}`
