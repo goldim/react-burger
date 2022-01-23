c11935d91caaefb49257b0305683bf94d004b291
zolotovdy <zolotovdy@ugpa.ru>
2022-01-23 22:55:39 +0300

    Добавлены состояния и тесты e2e

--- cypress/integration/burger-constructor/make-order.spec.js
+++ cypress/integration/burger-constructor/make-order.spec.js
@@ -1,6 +1,6 @@
 describe('making order', function() {
     before(function() {
-      cy.visit('http://localhost:3000');
+      cy.visit('http://localhost:3000/constructor');
     });
   
     it('should open burger constructor by default', function() {
@@ -18,6 +18,17 @@ describe('making order', function() {
       cy.get('@firstIngredient').trigger('dragstart');
       cy.get('@burgerConstructorArea').trigger('drop');
       
+      cy.intercept('POST', 'https://norma.nomoreparties.space/api/orders', { fixture: 'make-order.json' }).as('makeOrder');
       cy.get('button').contains("Оформить заказ").click();
+      // cy.wait('@makeOrder');
+
+      cy.intercept('POST', 'https://norma.nomoreparties.space/api/auth/login', { fixture: 'login.json' }).as('loginMethods');
+      cy.visit('http://localhost:3000/login');
+      cy.get('.input_type_email .input__icon-action').click();
+      cy.get('[name=email]').type('test@mail');
+      cy.get('[name=password]').type('test');
+      cy.get('button').contains('Войти').click().click();
+      
+      // cy.wait('@loginMethods');
     });
 }); 
